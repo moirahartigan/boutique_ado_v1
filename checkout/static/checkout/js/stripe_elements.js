bc28f9369bcba5f9aba82703b6bafa3904836624
moirahartigan <moirahartigan@gmail.com>
2021-11-22 13:14:39 +0000

    Added checkout form data caching in payment intent

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -59,27 +59,83 @@ form.addEventListener('submit', function(ev) {
     $('#submit-button').attr('disabled', true);
     $('#payment-form').fadeToggle(100);
     $('#loading-overlay').fadeToggle(100);
-    stripe.confirmCardPayment(clientSecret, {  // It uses the stripe.confirm card payment method to send the card information securely to stripe.
-        payment_method: {
-            card: card,
-        }
-    }).then(function(result) { // then execute this function on the result
-        if (result.error) {
-            var errorDiv = document.getElementById('card-errors'); // if there is an error we get an error message
-            var html = `
-                <span class="icon" role="alert">
-                <i class="fas fa-times"></i>
-                </span>
-                <span>${result.error.message}</span>`;
-            $(errorDiv).html(html); // if there is an error the user is allowed to fix it
-            $('#payment-form').fadeToggle(100);
-            $('#loading-overlay').fadeToggle(100);
-            card.update({ 'disabled': false});
-            $('#submit-button').attr('disabled', false);
-        } else {
-            if (result.paymentIntent.status === 'succeeded') { // if status come back as succeeded we submit the form
-                form.submit();
+
+    var saveInfo = Boolean($('#id-save-info').attr('checked'));
+    // From using {% csrf_token %} in the form
+    var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+    var postData = {
+        'csrfmiddlewaretoken': csrfToken,
+        'client_secret': clientSecret,
+        'save_info': saveInfo,
+    };
+    var url = '/checkout/cache_checkout_data/';
+
+    $.post(url, postData).done(function () {
+        stripe.confirmCardPayment(clientSecret, {  // It uses the stripe.confirm card payment method to send the card information securely to stripe.
+            payment_method: {
+                card: card,
+                billing_details: {
+                    name: $.trim(form.full_name.value),
+                    phone: $.trim(form.phone_number.value),
+                    email: $.trim(form.email.value),
+                    address:{
+                        line1: $.trim(form.street_address1.value),
+                        line2: $.trim(form.street_address2.value),
+                        city: $.trim(form.town_or_city.value),
+                        country: $.trim(form.country.value),
+                        state: $.trim(form.county.value),
+                    }
+                }
+            },
+            shipping: {
+                name: $.trim(form.full_name.value),
+                phone: $.trim(form.phone_number.value),
+                address: {
+                    line1: $.trim(form.street_address1.value),
+                    line2: $.trim(form.street_address2.value),
+                    city: $.trim(form.town_or_city.value),
+                    country: $.trim(form.country.value),
+                    postal_code: $.trim(form.postcode.value),
+                    state: $.trim(form.county.value),
+                }
+            },
+        }).then(function(result) { // then execute this function on the result
+            if (result.error) {
+                var errorDiv = document.getElementById('card-errors'); // if there is an error we get an error message
+                var html = `
+                    <span class="icon" role="alert">
+                    <i class="fas fa-times"></i>
+                    </span>
+                    <span>${result.error.message}</span>`;
+                $(errorDiv).html(html); // if there is an error the user is allowed to fix it
+                $('#payment-form').fadeToggle(100);
+                $('#loading-overlay').fadeToggle(100);
+                card.update({ 'disabled': false});
+                $('#submit-button').attr('disabled', false);
+            } else {
+                if (result.paymentIntent.status === 'succeeded') { // if status come back as succeeded we submit the form
+                    form.submit();
+                }
             }
-        }
-    });
+        });
+    }).fail(function () {  // failure function, which will be triggered
+        // if our view sends a 400 bad request response. And in that case, we'll just
+        // reload the page to show the user the error message from the view
+        // just reload the page, the error will be in django messages
+        location.reload();
+    })
 });
+
+// Summary of what happens
+// When the user clicks the submit button the event listener prevents the form from submitting
+// and instead disables the card element and triggers the loading overlay.
+// Then we create a few variables to capture the form data we can't put in
+// the payment intent here, and instead post it to the cache_checkout_data view
+// The view updates the payment intent and returns a 200 response, at which point we
+// call the confirm card payment method from stripe and if everything is ok
+// submit the form.
+// If there's an error in the form then the loading overlay will
+// be hidden the card element re-enabled and the error displayed for the user.
+// If anything goes wrong posting the data to our view. We'll reload the page and
+// display the error without ever charging the user.
+// https://learn.codeinstitute.net/courses/course-v1:CodeInstitute+EA101+2021_T1/courseware/eb05f06e62c64ac89823cc956fcd8191/48ac02aa8ecc4079be016c336231bee7/
