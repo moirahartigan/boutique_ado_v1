6fddda32c4dba65a403d73f5d1d2fa7980d94169
moirahartigan <moirahartigan@gmail.com>
2021-11-23 13:13:21 +0000

    Updated checkout form

--- checkout/forms.py
+++ checkout/forms.py
@@ -20,20 +20,20 @@ class OrderForm(forms.ModelForm):
             'full_name': 'Full Name',
             'email': 'Email Address',
             'phone_number': 'Phone Number',
-            'country': 'Country',
             'postcode': 'Postal Code',
             'town_or_city': 'Town or City',
             'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
-            'county': 'County',
+            'county': 'County, State or Locality',
         }
 
         self.fields['full_name'].widget.attrs['autofocus'] = True # set the autofocus attribute on the full name field to true so the cursor will start in the full name field when the user loads the page.
         for field in self.fields: #  iterate through the forms fields
-            if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *' # adding a star to the placeholder if it's a required field on the model.
-            else:
-                placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder'] = placeholder # and Setting all the placeholder attributes to their values in the dictionary above
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *' # adding a star to the placeholder if it's a required field on the model.
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder # and Setting all the placeholder attributes to their values in the dictionary above
             self.fields[field].widget.attrs['class'] = 'stripe-style-input' # adding a css class to use later
             self.fields[field].label = False # then removing the form fields labels. Since we won't need them given the placeholders are now set.
